266fe6c327c4b8757e14ebe7405e2a776a43a88c
Diego Rubio <diego.rubio@spr.gob.mx>
2022-05-06 14:35:27 -0500

    acomodando y enumerando tablas

--- resources/views/pagina/lista-contratos.blade.php
+++ resources/views/pagina/lista-contratos.blade.php
@@ -6,9 +6,37 @@
 @stop
 
 @section('content')
+<h1>Lista de Contratos</h1>
 
-    <h1>Lista de Contratos</h1>
-    <ul class="list-group list-group-horizontal list-group-item-action row-cols-6 list-group-item-action">
+    <table id="contratos" class="table table-striped mt-4 table-bordered shadow-lg" style="width:100%">
+      <thead>
+        <tr>
+          <th scope="col">Proveedor</th>
+          <th scope="col">Fecha de inicio</th>
+          <th scope="col">Proveedor</th>
+          <th scope="col">Fecha de corte</th>
+          <th scope="col">Numero de contrato</th>
+          <th scope="col">Importe Mensual</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($contratos as $contrato )
+
+        <tr>
+          <td>{{$contrato->id}}</td>
+          <td>{{$contrato->fecha_inicio}}</td>
+          <td>{{$contrato->proveedor}}</td>
+          <td>{{$contrato->dia_corte_mensual}}</td>
+          <td>{{$contrato->num_contrato}}</td>
+          <td>{{$contrato->importe_mensual}}</td>
+        </tr>
+
+        @endforeach
+
+      </tbody>
+    </table>
+
+{{--     <ul class="list-group list-group-horizontal list-group-item-action row-cols-6 list-group-item-action">
 
         <li class="list-group-item bg-primary text-white">Proveedor</li>
         <li class="list-group-item bg-primary text-white">Fecha de inicio</li>
@@ -28,16 +56,25 @@
         <li class="list-group-item">{{$contrato->importe_mensual}}</li>
       </ul>
     @endforeach
-
+ --}}
 @stop
 
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <link href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" rel="stylesheet" crossorigin="anonymous">
 @stop
 
 @section('js')
+<script>
+$(document).ready(function() {
+  $('#contratos').DataTable();
+} );
+</script>
+
+<script src="https://code.jquery.com/jquery-3.5.1.js" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"  crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 @stop
--- resources/views/pagina/lista-pagos.blade.php
+++ resources/views/pagina/lista-pagos.blade.php
@@ -7,9 +7,38 @@
  @stop
  
  @section('content')
+
     <h1>Lista de Pagos</h1>
 
-    <ul class="list-group list-group-horizontal list-group-item-action row-cols-6 list-group-item-action">
+    <table id="contratos" class="table table-striped mt-4 table-bordered shadow-lg" style="width:100%">
+      <thead>
+        <tr>
+          <th scope="col">Fecha de solicitud</th>
+          <th scope="col">Fecha de pago</th>
+          <th scope="col">Numero de recibo de factura</th>
+          <th scope="col">Contrato</th>
+          <th scope="col">Periodo Pago</th>
+          <th scope="col">Monto</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($pagos as $pago )
+
+        <tr>
+          <td>{{$pago->fecha_solicitud}}</td>
+          <td>{{$pago->fecha_pago}}</td>
+          <td>{{$pago->num_recibo_factura}}</td>
+          <td>{{$pago->contrato}}</td>
+          <td>{{$pago->periodo_pago}}</td>
+          <td>{{$pago->monto}}</td>
+
+        </tr>
+
+        @endforeach
+
+      </tbody>
+    </table>
+{{--     <ul class="list-group list-group-horizontal list-group-item-action row-cols-6 list-group-item-action">
 
       <li class="list-group-item bg-primary text-white">Fecha de solicitud</li>
       <li class="list-group-item bg-primary text-white">Fecha de pago</li>
@@ -28,19 +57,29 @@
       <li class="list-group-item">{{$pago->periodo_pago}}</li>
       <li class="list-group-item">{{$pago->monto}}</li>
       <li class="list-group-item">{{$pago->monto}}</li>
-    </ul>
+    </ul> 
 
     @endforeach
+--}}
+ @stop
+ 
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" rel="stylesheet" crossorigin="anonymous">
+@stop
+ 
 
- @stop
- 
- @section('css')
-     <link rel="stylesheet" href="/css/admin_custom.css">
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
- 
- @stop
- 
- @section('js')
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
- 
- @stop
+@section('js')
+<script>
+$(document).ready(function() {
+  $('#contratos').DataTable();
+} );
+</script>
+
+<script src="https://code.jquery.com/jquery-3.5.1.js" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"  crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+@stop
