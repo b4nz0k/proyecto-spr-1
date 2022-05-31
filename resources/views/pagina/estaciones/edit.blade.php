6c98bbfb1c78035bda0d25c298ca979cb496e0b0
Diego Rubio <diego.rubio@spr.gob.mx>
2022-05-31 13:38:22 -0500

    se agrego un model que faltaba e la edicion de estaciones en el boton de eliminar

--- resources/views/pagina/estaciones/edit.blade.php
+++ resources/views/pagina/estaciones/edit.blade.php
@@ -15,9 +15,29 @@
       <h1>Editar Estacion</h1>
         <div class="col-auto">
           <a href="/historial-estacion/{{$estacion->id}}" class="btn btn-primary">Ver Historial</a>
-          <button href="/eliminar-estacion/{{$estacion->id}}" class="btn btn-danger">Eliminar</button>
+          <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModalCenter">Eliminar</button>
         </div><hr>
-
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Advertencia</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    Estas seguro que Desea eliminar?
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    <a href="/eliminar-estacion/{{$estacion->id}}" class="btn btn-danger">Eliminar</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {{-- End modal --}}
       <div class="mb-3 col-md-4 col-auto">
           <label for="">Ciudad</label>
           <select class="form-select" aria-label="Default select example" name="ciudad">
